116835ab9d2cf2f1be9ae103aa83779437feeacc
Aayush-99 <aayush.88@outlook.com>
2024-10-01 17:28:49 -0500

    Forms Cleaned up

--- forms.py
+++ forms.py
@@ -1,52 +1,58 @@
 from wtforms import Form, StringField, SelectField, validators
 
+# Common validator for required fields
+required = [validators.input_required()]
 
+# Employee Form
 class EmployeeForm(Form):
-    firstname = StringField(u'First Name', validators=[validators.input_required()])
-    lastname = StringField(u'Last Name', validators=[validators.input_required()])
-    address1 = StringField(u'Address 1', validators=[validators.input_required()])
-    address2 = StringField(u'Address 2', validators=[validators.optional()])
-    city = StringField(u'City', validators=[validators.input_required()])
-    state = StringField(u'State', validators=[validators.input_required()])
-    zipcode = StringField(u'Zip Code', validators=[validators.input_required()])
-    ssn = StringField(u'Social Security Number (numbers only)', validators=[validators.input_required()])
-    witholdings = StringField(u'Witholdings', validators=[validators.input_required()])
-    salary = StringField(u'Salary (Dollars)', validators=[validators.input_required()])
+    firstname = StringField('First Name', validators=required)
+    lastname = StringField('Last Name', validators=required)
+    address1 = StringField('Address 1', validators=required)
+    address2 = StringField('Address 2', validators=[validators.optional()])
+    city = StringField('City', validators=required)
+    state = StringField('State', validators=required)
+    zipcode = StringField('Zip Code', validators=required)
+    ssn = StringField('Social Security Number (numbers only)', validators=required)
+    witholdings = StringField('Witholdings', validators=required)
+    salary = StringField('Salary (Dollars)', validators=required)
 
-
+# Customer Form
 class CustomerForm(Form):
-    companyname = StringField(u'Company Name', validators=[validators.input_required()])
-    firstname = StringField(u'First Name', validators=[validators.input_required()])
-    lastname = StringField(u'Last Name', validators=[validators.input_required()])
-    address1 = StringField(u'Address 1', validators=[validators.input_required()])
-    address2 = StringField(u'Address 2', validators=[validators.optional()])
-    city = StringField(u'City', validators=[validators.input_required()])
-    state = StringField(u'State', validators=[validators.input_required()])
-    zipcode = StringField(u'Zip Code', validators=[validators.input_required()])
-    price = StringField(u'Price (Dollars)', validators=[validators.input_required()])
+    companyname = StringField('Company Name', validators=required)
+    firstname = StringField('First Name', validators=required)
+    lastname = StringField('Last Name', validators=required)
+    address1 = StringField('Address 1', validators=required)
+    address2 = StringField('Address 2', validators=[validators.optional()])
+    city = StringField('City', validators=required)
+    state = StringField('State', validators=required)
+    zipcode = StringField('Zip Code', validators=required)
+    price = StringField('Price (Dollars)', validators=required)
 
-
+# Vendor Form
 class VendorForm(Form):
-    companyname = StringField(u'Company Name', validators=[validators.input_required()])
-    part = StringField(u'Part', validators=[validators.input_required()])
-    price_per_unit = StringField(u'Price/Unit (Dollars)', validators=[validators.input_required()])
-    address1 = StringField(u'Address 1', validators=[validators.input_required()])
-    address2 = StringField(u'Address 2', validators=[validators.optional()])
-    city = StringField(u'City', validators=[validators.input_required()])
-    state = StringField(u'State', validators=[validators.input_required()])
-    zipcode = StringField(u'Zip Code', validators=[validators.input_required()])
+    companyname = StringField('Company Name', validators=required)
+    part = StringField('Part', validators=required)
+    price_per_unit = StringField('Price/Unit (Dollars)', validators=required)
+    address1 = StringField('Address 1', validators=required)
+    address2 = StringField('Address 2', validators=[validators.optional()])
+    city = StringField('City', validators=required)
+    state = StringField('State', validators=required)
+    zipcode = StringField('Zip Code', validators=required)
 
-
+# Employee Payment Form
 class EmployeePaymentForm(Form):
-    employee = SelectField(u'Employee')
+    employee = SelectField('Employee')
 
+# Purchase Order Form
 class POForm(Form):
-    part = SelectField(u'Part')
-    quantity = StringField(u'Quantity', validators=[validators.input_required()])
+    part = SelectField('Part')
+    quantity = StringField('Quantity', validators=required)
 
+# Invoice Form
 class InvoiceForm(Form):
-    customer = SelectField(u'Customer')
-    number_to_invoice = StringField(u'Number of Units to Invoice', validators=[validators.input_required()])
-    
+    customer = SelectField('Customer')
+    number_to_invoice = StringField('Number of Units to Invoice', validators=required)
+
+# Build Inventory Form
 class BuildInventoryForm(Form):
-    quantity = StringField(u'Quantity', validators=[validators.input_required()])
+    quantity = StringField('Quantity', validators=required)
